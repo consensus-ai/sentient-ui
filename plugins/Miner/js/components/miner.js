dc9998b1f4ddb282620c9993212b8f0157b53c06
sergey.bystranovsky.triare <sergey.bystranovsky.triare@gmail.com>
2018-10-30 18:43:31 +0200

    Update miner.js

--- plugins/Miner/js/components/miner.js
+++ plugins/Miner/js/components/miner.js
@@ -1,73 +1,98 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { ToastContainer, Slide } from 'react-toastify'
+import {ToastContainer, Slide} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-const Miner = ({walletLocked, miningStatus, actions }) => {
-  const getHashRateForDisplay = () => {
-    if (!miningStatus.cpumining) {
-      return "0 KH/s"
-    }
-    return (miningStatus.cpuhashrate / 1000).toFixed(2) + " KH/s"
-  }
-
-  const miningActionOnClick = (e) => {
-    if (miningStatus.cpumining) {
-      actions.stopMiner()
-    } else {
-      actions.startMiner()
-    }
-  }
-
-  return (
-    <div className="miner">
-      <div className="content">
-        { walletLocked &&
-          <div className="locked-status-container">
-            <i className="fa fa-exclamation-triangle"></i>
-            <span>Wallet is locked. You must unlock it before mining.</span>
-          </div>
+const Miner = ({walletLocked, miningStatus, actions}) => {
+    const getHashRateForDisplay = () => {
+        if (!miningStatus.cpumining) {
+            return "0 KH/s"
         }
+        return (miningStatus.cpuhashrate / 1000).toFixed(2) + " KH/s"
+    }
 
-        { !walletLocked &&
-          <div className="wallet-controls-container">
-            <div className="row blocks-mined-container">
-              <div className="col col-1 label blocks-mined-label">Blocks Mined:</div>
-              <div className="col col-2 value blocks-mined-value">{miningStatus.blocksmined}</div>
-            </div>
-            <div className="row stale-blocks-container">
-              <div className="col col-1 label stale-blocks-label">Stale Blocks Mined:</div>
-              <div className="col col-2 value stale-blocks-value">{miningStatus.staleblocksmined}</div>
-            </div>
-
-            <div className="divider"></div>
-
-            <div className="row miner-status-container">
-              <div className="col col-1 label miner-status-label">CPU Mining:</div>
-              <div className={"col col-2 value miner-status-value " + (miningStatus.cpumining ? "enabled" : "disabled")}>
-                {miningStatus.cpumining ? "ON" : "OFF"}
-              </div>
-            </div>
-
-            <div className="row hash-rate-container">
-              <div className="col col-1 label hash-rate-label">CPU Hash Rate:</div>
-              <div className="col col-2 value hash-rate-value">{getHashRateForDisplay()}</div>
-            </div>
-
-            <div className={"button mining-action-button " + (miningStatus.cpumining ? "stop" : "start")} onClick={miningActionOnClick}>
-              {miningStatus.cpumining ? "Stop CPU Miner" : "Start CPU Miner"}
-            </div>
-          </div>
+    const miningActionOnClick = (e) => {
+        if (miningStatus.cpumining) {
+            actions.stopMiner()
+        } else {
+            actions.startMiner()
         }
-      </div>
-    </div>
-  )
+    }
+
+    return (
+        <div className="miner">
+            <div className="content space-between">
+
+                <div className="wallet"><b>1,500</b> SEN</div>
+
+                {walletLocked &&
+                <div className="top-row">
+                    <div className="exclamation"><i className="fa fa-exclamation-triangle"></i> Wallet is locked. You
+                        must
+                        unlock it before mining
+                    </div>
+                    <div className="dropdown">
+                        <span>Local Daemon <i className="fa fa-angle-down"></i></span>
+                        <ul>
+                            <li>Pool</li>
+                            <li>Local Daemon</li>
+                        </ul>
+                    </div>
+                </div>
+                }
+
+                {!walletLocked &&
+
+                <div className="data-cards">
+                    <div className="item" passive>
+                        <b>-</b>
+                        <small></small>
+                        <span>Current hash rate</span>
+                    </div>
+                    <div className="item" passive>
+                        <b>58.4 GH/s</b>
+                        <small></small>
+                        <span>Pool hash rate</span>
+                    </div>
+                    <div className="item">
+                        <b>98.88%</b>
+                        <small>1.12% rejected</small>
+                        <span>Shares</span>
+                    </div>
+                    <div className="item" passive>
+
+                        <div className="balance">
+                            <i className="fa fa-info-circle"></i>
+                            <div className="info">Minimum payout: 0.1 SEN</div>
+                        </div>
+
+                        <b>500 SEN</b>
+                        <small></small>
+                        <span>Unpaid balance</span>
+                    </div>
+                </div>
+
+                }
+
+
+                {!walletLocked &&
+
+                <div className="graph">
+                    <img src="assets/Graph.svg" />
+                </div>
+
+
+                }
+
+            </div>
+        </div>
+    )
 }
 
 Miner.propTypes = {
-  walletLocked: PropTypes.bool.isRequired,
-  miningStatus: PropTypes.object.isRequired,
+    walletLocked: PropTypes.bool.isRequired,
+    miningStatus: PropTypes.object.isRequired,
 }
 
 export default Miner

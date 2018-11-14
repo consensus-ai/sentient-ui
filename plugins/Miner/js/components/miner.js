a3b30fdd40c9fb3d2bf58225897176ab4a33d21a
sergey.bystranovsky.triare <sergey.bystranovsky.triare@gmail.com>
2018-11-14 22:38:52 +0200

    Miner new design

--- plugins/Miner/js/components/miner.js
+++ plugins/Miner/js/components/miner.js
@@ -24,44 +24,53 @@ const Miner = ({walletLocked, miningStatus, confirmedBalance, synced, actions}) 
 
     return (
         <div className="miner">
+            <div className="balance-info-container">
+                <div className="balance-info">
+                    <div className="balance-info-icon balance-info-synced-icon" title=""></div>
+                    <div className="balance-info-amount-container" title="0 SEN pending"><span
+                        className="balance-info-amount">{formattedConfirmedBalance}</span><span className="balance-info-currency">SEN</span></div>
+                </div>
+            </div>
+
             <div className="content space-between">
 
-                <div className="wallet"><b>{formattedConfirmedBalance}</b> SEN</div>
                 <div className="top-row">
-                    <div className="exclamation" style={walletLocked ? {} : {display: "none"}}><i className="fa fa-exclamation-triangle"></i>
-                        Wallet is locked. You must unlock it before mining
-                    </div>
                     <PoolDropdownView />
+                    <div className="button miner-button stop-button">
+                        <div className="button-icon"></div>
+                        <span>Start miner</span>
+                    </div>
+
                 </div>
                 <div className="data-cards">
-                    <div className="item">
-                        <b>-</b>
+                    <div className="item"><b>&#8211;</b>
                         <small></small>
-                        <span>Current hash rate</span>
-                    </div>
-                    <div className="item">
-                        <b>58.4 GH/s</b>
+                        <span>Current hash rate</span></div>
+                    <div className="item" hidden><b>58.4 GH/s</b>
                         <small></small>
-                        <span>Pool hash rate</span>
-                    </div>
+                        <span>Pool hash rate</span></div>
+                    <div className="item" disabled><b>98.88%</b>
+                        <small className="red">1.12% rejected</small>
+                        <span>Shares</span></div>
                     <div className="item">
-                        <b>98.88%</b>
-                        <small>1.12% rejected</small>
-                        <span>Shares</span>
-                    </div>
-                    <div className="item">
-                        <div className="balance">
-                            <i className="fa fa-info-circle"></i>
+                        <div className="balance"><i className="fa fa-info-circle"></i>
                             <div className="info">Minimum payout: 0.1 SEN</div>
                         </div>
-
                         <b>500 SEN</b>
                         <small></small>
-                        <span>Unpaid balance</span>
-                    </div>
+                        <span>Unpaid balance</span></div>
                 </div>
                 <div className="graph">
-                    <img src="assets/Graph.svg" />
+                    <div className="wrap"></div>
+                    <div className="footer">
+                        <span>Shares this session</span>
+                        <ul>
+                            <li>24 hours</li>
+                            <li className="active">3 days</li>
+                            <li>1 week</li>
+                            <li>1 month</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>

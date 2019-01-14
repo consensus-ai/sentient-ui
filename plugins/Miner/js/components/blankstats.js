b9931a2eb98a58fba67fec323b1dcf0022b82c92
Alexander Belonogov <belonogov@artistsoft.com>
2019-01-14 14:23:52 +0200

    Bugfix: Use default path for sentient miner.

--- plugins/Miner/js/components/blankstats.js
+++ plugins/Miner/js/components/blankstats.js
@@ -6,7 +6,7 @@ const BlankStats = () => {
             <div className="item" disabled>
                 <b>&#8211;</b>
                 <small></small>
-                <span>Current hash rate</span></div>
+                <span>Current Hash Rate</span></div>
             <div className="item" hidden></div>
             <div className="item" disabled>
                 <b><b>&#8211;</b></b>
@@ -17,14 +17,14 @@ const BlankStats = () => {
                 <div className="balance">
                     <i className="fa fa-info-circle"></i>
                     <div className="info">
-                        Minimum payout: 25 SEN
+                        Minimum Payout: 25 SEN
                         <br/>
                         Payout Frequency: 24hrs
                     </div>
                 </div>
                 <b><span>&#8211;</span><span> SEN</span></b>
                 <small></small>
-                <span>Unpaid balance</span>
+                <span>Unpaid Balance</span>
             </div>
         </div>
     )
